77312029888750f79252dabe7c3e195a349466bb
Jose Fletes <joseangelfletes@gmail.com>
2023-01-22 12:41:00 -0800

    add associations

--- app/models/recital.rb
+++ app/models/recital.rb
@@ -1,2 +1,5 @@
 class Recital < ApplicationRecord
+  has_many :students
+  has_many :tickets
+  has_many :users, through: :tickets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_22_203051) do
+ActiveRecord::Schema.define(version: 2023_01_22_203509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,23 @@ ActiveRecord::Schema.define(version: 2023_01_22_203051) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.float "price"
     t.integer "quantity", default: 1
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
