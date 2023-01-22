c3487f003b2bc99ea6b9122e544bc07383270547
Jose Fletes <joseangelfletes@gmail.com>
2023-01-22 12:54:54 -0800

    add routes using rest convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   
-  resources :students
-  resources :users
-  resources :tickets
+  resources :students, only: [:index]
+  resources :users, only: [:show, :create]
+  resources :tickets, only: [:create, :update]
   resources :recitals
-  get "/recitals", to: "recitals#index"
-  get "/recitals/:id", to: "recitals#show"
+
+  #Custom Routes
+
+  # get "/recitals", to: "recitals#index"
+  # get "/recitals/:id", to: "recitals#show"
+
+
+
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
