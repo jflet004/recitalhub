f670259c9e98adbe7d583c7a4627779493cc2e58
Jose Fletes <joseangelfletes@gmail.com>
2023-01-26 07:49:23 -0800

    set errors for data fetching

--- client/src/RecitalList.js
+++ client/src/RecitalList.js
@@ -3,20 +3,30 @@ import RecitalCard from './RecitalCard'
 
 const RecitalList = () => {
   const [recitals, setRecitals] = useState([])
+  const [errors, setErrors] = useState(false)
 
   useEffect(() => {
     fetch("/recitals")
-      .then(r => r.json())
-      .then(data => setRecitals(data))
-      .catch(error => console.log(error))
+      .then(r => {
+        if (r.ok) {
+          r.json().then(data => setRecitals(data))
+        }
+        else {
+          r.json().then(data => setErrors(data.error))
+        }
+      })
   }, [])
+
+  const recitalList = recitals.map(recital => (
+    <RecitalCard key={recital.id} recital={recital} />
+  ))
+
+  if(errors) return <h1>{errors}</h1>
 
   return (
     <ul>
       <h1>Recital List Here</h1>
-      {recitals.map(recital => (
-          <RecitalCard key={recital.id} recital={recital} />
-      ))}
+      {recitalList}
     </ul>
   )
 }
